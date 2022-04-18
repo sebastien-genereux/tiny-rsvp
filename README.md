e99867c844daf444b8f308336d89ef461e7ac490
sebastien-genereux <sebastiengenereux12@gmail.com>
2022-04-17 20:55:48 -0400

    initial web app upload

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # tiny-rsvp
 
-A web application to collect RSVPs for an event.
+A web application to collect RSVPs for an event. Populate your event details in *configs/* and update the *configPath* setting at the top of *main.go*! - currently set to an example event. Timestamps for RSVP window start and end are expected in RFC3339 format.
+
+The data entry form, as defined by *web/templates/rsvp.html* will ask for the attendees' family name, number of attendees, contact info and any comments (ex. allergies).
+
+Those details are then saved to a timestamped .csv saved in databases/ which you will need to fetch (ex. via scp) to see you attendee list. At the top of the database file is a header, defined by *totalHeader* which keeps a running total of attendees for you. Input validation is done client side through form types in the *web/templates/rsvp.html*
+
+Feel free to fork, add/change features as desired. The intent was the features minimal but useful.
--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"io/ioutil"
-	"html/template"
+	"os"
+	"time"
+	"strconv"
+	"fmt"
+	"regexp"
+
 	"gopkg.in/yaml.v3"
 )
 
+// eventConfig is a struct used to unpack the event configuration yaml
 type eventConfig struct {
 	EventName	string `yaml:"Event-name"`
 	Date		string `yaml:"Date"`
@@ -17,38 +22,124 @@ type eventConfig struct {
 	Description	string `yaml:"Description"`
 	HostName	string `yaml:"Host-name"`
 	Contact		string `yaml:"Contact"`
+	StartStr	string `yaml:"RSVP-start"`
+	EndStr		string `yaml:"RSVP-end"`
+	StartTime	time.Time
+	EndTime		time.Time
 }
 
-func rsvpHandler(w http.ResponseWriter, r *http.Request, conf eventConfig) {
-    t, _ := template.ParseFiles("./web/templates/rsvp.html")
-	t.Execute(w, conf)  // should later swap nil for parametrized event details
+/* SETTINGS */
+
+// totalHeader defines the label used to keep track of the total attendees at the top of the database file
+const totalHeader string = "Total number of attendees: "
+
+// configPath contains the relative or absolute path to the event configuration being used to parse event details
+const configPath string = "./configs/event_example.yml"
+
+// serverPort indicates which tcp port to listen on and server http
+const serverPort string = "8080"
+
+/* GLOBALS */ 
+
+// configData saves the event configuration as global state to share with the http handlers. It is initialized in main, before any http is served.
+var configData eventConfig
+
+// databaseFile stores a pointer to the database file as global state to share with the http handlers. It is initialized in main, before any http is served.
+var databaseFile *os.File
+
+// headerReg stores a regular expression used to match on the total attendee header and submatch on the exact current count stored. It is saved as part of the global scope to share with the http handlers. It is compiled in main, before any http is served.
+var headerReg *regexp.Regexp
+
+func rsvpHandler(w http.ResponseWriter, r *http.Request) {
+	// Serve the appropriate page based on the time relative to the RSVP window
+	var t *template.Template
+	now := time.Now()
+	if now.Before(configData.StartTime) {
+		t, _ = template.ParseFiles("./web/templates/early.html")
+	}else if now.After(configData.EndTime){
+		t, _ = template.ParseFiles("./web/templates/late.html")
+	}else {
+		t, _ = template.ParseFiles("./web/templates/rsvp.html")
+	}
+
+	t.Execute(w, configData)
+}
+
+func thanksHandler(w http.ResponseWriter, r *http.Request) {
+    t, _ := template.ParseFiles("./web/templates/thanks.html")
+	t.Execute(w, nil) 
 }
 
 func submitHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-}
+	// Record submission details in the database
+	fmt.Fprintf(databaseFile, "%s,%s,%s,%s\n", r.FormValue("name"), r.FormValue("count"), r.FormValue("contact"), r.FormValue("comments"))
 
-func makeRsvpHandler(fn func(http.ResponseWriter, *http.Request, eventConfig)) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		configFile, err := ioutil.ReadFile("./configs/event.yml")
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	
-		var configData eventConfig
-		err2 := yaml.Unmarshal(configFile, &configData)
-	
-		if err2 != nil {
-			log.Fatal(err2)
-		}
-
-		fn(w, r, configData)
+	// Update the total attendee header, by reading the first line, grabbing the first submatch (i.e the current count) and adding the new attendee count. Lastly update the header. Note this stops counting past '9'*20 attendees due to the byte array size
+	currentHeader := make([]byte, len(totalHeader) + 20)
+	_, err := databaseFile.ReadAt(currentHeader, 0)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
 	}
+
+	total, err2 := strconv.Atoi(headerReg.FindStringSubmatch(string(currentHeader))[1])
+	if err2 != nil {
+		http.Error(w, err2.Error(), http.StatusInternalServerError)
+        return
+	}
+
+	oldTotal, err3 := strconv.Atoi(r.FormValue("count"))
+	if err3 != nil {
+		http.Error(w, err3.Error(), http.StatusInternalServerError)
+        return
+	}
+
+	newTotal := strconv.Itoa(total + oldTotal)
+	databaseFile.WriteAt([]byte(totalHeader + newTotal + "\n"), 0)
+
+	http.Redirect(w, r, "/thanks", http.StatusFound)
 }
 
 func main() {
-    http.HandleFunc("/", makeRsvpHandler(rsvpHandler))
+	// Read the config file
+	configFile, err := os.ReadFile(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Parse the configuration file and unmarshal into global config variable
+	err2 := yaml.Unmarshal(configFile, &configData)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	// Convert the rfc3339 time strings into time structs
+	startTime, err := time.Parse(time.RFC3339, configData.StartStr)
+	endTime, err2 := time.Parse(time.RFC3339, configData.EndStr)
+	if err != nil || err2 != nil{
+		log.Fatal(err, err2)
+	}
+	configData.StartTime, configData.EndTime = startTime, endTime
+	
+	// Create a timestamped database file and save a pointer to the global database variable
+	databaseName := "./databases/" + strconv.Itoa(int(time.Now().Unix())) + "_rsvp_" + configData.EventName + ".csv"
+	databaseLocal, err2 := os.Create(databaseName)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	databaseFile = databaseLocal
+	defer databaseFile.Close()
+
+	// Write the header info in the database file
+	databaseFile.WriteString(totalHeader + "0\n")
+	fmt.Fprintf(databaseFile, "%s,%s,%s,%s\n", "Family Name", "Count of Attendees", "Contact Info", "Comments")
+
+	// Compile the header regex for later use by the submit handler
+	headerReg = regexp.MustCompile(totalHeader + "([0-9]+)")
+
+	// Serve the web application
+    http.HandleFunc("/", rsvpHandler)
 	http.HandleFunc("/submit", submitHandler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    http.HandleFunc("/thanks", thanksHandler)
+	log.Fatal(http.ListenAndServe(":" + serverPort, nil))
 }
